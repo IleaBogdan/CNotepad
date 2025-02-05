95d3b160d7174b8f13dbd4ca791bc6e9b84f05b7
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-02-05 17:16:35 +0200

    saving text in file

--- cnotepad.c
+++ cnotepad.c
@@ -1,26 +1,35 @@
 #include <gtk/gtk.h>  // GTK lib
 #include "fileIO\fileIO.h"
 
-void onclick(GtkWidget* button, gpointer user_data){
-    saveTextInFile("N");
+
+GtkWidget *text_view;
+
+void saveText(GtkWidget* button, gpointer user_data){
+    GtkTextIter start, end;
+    GtkTextBuffer* buffer=gtk_text_view_get_buffer(text_view);
+    gchar* text;
+    gtk_text_buffer_get_bounds(buffer, &start, &end);
+    text=gtk_text_buffer_get_text(buffer, &start, &end, FALSE);
+    saveTextInFile(text);
+    g_free(text);
 }
 
-static void activate(GtkApplication *app, gpointer user_data) {
+void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "C Notepad");
     gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
 
     gtk_container_set_border_width(GTK_CONTAINER(window), 5);
 
-    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 3);
     gtk_container_add(GTK_CONTAINER(window), box);
     
-    GtkWidget *text_view = gtk_text_view_new();
-    gtk_box_pack_start(GTK_BOX(box), text_view, TRUE, TRUE, 5);
-
     GtkWidget *button = gtk_button_new_with_label("Save Text");
-    g_signal_connect(button, "clicked", G_CALLBACK(onclick), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(saveText), NULL);
     gtk_box_pack_start(GTK_BOX(box), button, FALSE, FALSE, 5);
+
+    text_view = gtk_text_view_new();
+    gtk_box_pack_start(GTK_BOX(box), text_view, TRUE, TRUE, 5);
 
     gtk_widget_show_all(window);
 }
--- fileIO/fileO.c
+++ fileIO/fileO.c
@@ -1,6 +1,12 @@
 #include "fileIO.h"
 
 void saveTextInFile(const char *text){
-    freopen("save.txt", "w", stdout);
-    printf("Hello?");    
+    if (text){
+        FILE* file=fopen("save.txt", "w");
+        if (file){
+            fprintf(file, "%s", text);
+            fclose(file);
+            printf("Saved\n");
+        } else printf("Failed file opening\n"); 
+    } else printf("Null text\n");
 }
